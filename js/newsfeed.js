c47185bca73abe55a2f8bb391307b140bf5e89a9
cellay <camposellay@gmail.com>
2018-01-14 17:42:56 -0500

    Anakin

--- js/newsfeed.js
+++ js/newsfeed.js
@@ -22,8 +22,7 @@ var config = {
             });
             $('.box').append('<img class="uplImg" src="" alt="">')
             console.log(this);
-            alert(this);
-            $('.uplImg').attr('src', img);
+            $(this).attr('src', img);
           };
           watcher.readAsDataURL(this.files[0])
       }
